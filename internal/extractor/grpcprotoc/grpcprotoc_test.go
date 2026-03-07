2e315ac94f83a673bb3ea5fc2f1edd178378a349
spencercjh <spencercjh@gmail.com>
2026-03-08 00:33:09 +0800

    feat(grpc): create grpcprotoc package structure
    Add package constants and Info type for gRPC-protoc extractor.
    
    Signed-off-by: Claude <claude@anthropic.com>
    Signed-off-by: spencercjh <spencercjh@gmail.com>

---
+++ internal/extractor/grpcprotoc/grpcprotoc_test.go
@@ -0,0 +1,41 @@
+package grpcprotoc
+
+import (
+	"testing"
+)
+
+func TestFrameworkName(t *testing.T) {
+	if FrameworkName != "grpc-protoc" {
+		t.Errorf("FrameworkName = %q, want %q", FrameworkName, "grpc-protoc")
+	}
+}
+
+func TestInfo(t *testing.T) {
+	info := Info{
+		ProtoFiles:   []string{"test.proto"},
+		ProtoRoot:    "/proto",
+		HasGoogleAPI: true,
+		HasBuf:       false,
+		ImportPaths:  []string{"/proto", "/usr/local/include"},
+	}
+
+	if len(info.ProtoFiles) != 1 {
+		t.Errorf("len(ProtoFiles) = %d, want 1", len(info.ProtoFiles))
+	}
+
+	if info.ProtoRoot != "/proto" {
+		t.Errorf("ProtoRoot = %q, want %q", info.ProtoRoot, "/proto")
+	}
+
+	if !info.HasGoogleAPI {
+		t.Error("HasGoogleAPI should be true")
+	}
+
+	if info.HasBuf {
+		t.Error("HasBuf should be false")
+	}
+
+	if len(info.ImportPaths) != 2 {
+		t.Errorf("len(ImportPaths) = %d, want 2", len(info.ImportPaths))
+	}
+}
