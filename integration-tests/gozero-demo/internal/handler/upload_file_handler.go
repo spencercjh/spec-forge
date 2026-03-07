23b31d460da25ec4a7eac2d207961d08a2c59425
spencercjh <spencercjh@gmail.com>
2026-03-07 19:13:34 +0800

    feat(gozero-demo): generate complete go-zero framework code with goctl
    Signed-off-by: spencercjh <spencercjh@gmail.com>

---
+++ integration-tests/gozero-demo/internal/handler/upload_file_handler.go
@@ -0,0 +1,24 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package handler
+
+import (
+	"net/http"
+
+	"github.com/spencercjh/gozero-demo/internal/logic"
+	"github.com/spencercjh/gozero-demo/internal/svc"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func UploadFileHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		l := logic.NewUploadFileLogic(r.Context(), svcCtx)
+		resp, err := l.UploadFile()
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
---
+++ integration-tests/gozero-demo/internal/logic/list_users_logic.go
@@ -0,0 +1,33 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package logic
+
+import (
+	"context"
+
+	"github.com/spencercjh/gozero-demo/internal/svc"
+	"github.com/spencercjh/gozero-demo/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ListUsersLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewListUsersLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ListUsersLogic {
+	return &ListUsersLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ListUsersLogic) ListUsers(req *types.ListUsersRequest) (resp *types.UserListResponse, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
